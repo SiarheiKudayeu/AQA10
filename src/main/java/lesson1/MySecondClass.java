be0d18693c4a7979d2b0bd9892e81bdbe863d11d
Sergey <anduser@PLWRS-CND6212VLC.andersen-technology.by>
2024-01-24 20:13:13 +0300

    changed second class

--- src/main/java/lesson1/MySecondClass.java
+++ src/main/java/lesson1/MySecondClass.java
@@ -2,6 +2,6 @@ package lesson1;
 
 public class MySecondClass {
     public static void main(String[] args) {
-        System.out.println("MySecondClass");
+        System.out.println("MySecond");
     }
 }
