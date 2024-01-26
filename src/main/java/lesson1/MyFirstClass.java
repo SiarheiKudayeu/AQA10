2f03b121818473773406bc560b4fd17e3628fd34
Sergey <anduser@PLWRS-CND6212VLC.andersen-technology.by>
2024-01-26 20:52:28 +0300

    lesson1

--- src/main/java/lesson1/MyFirstClass.java
+++ src/main/java/lesson1/MyFirstClass.java
@@ -2,6 +2,6 @@ package lesson1;
 
 public class MyFirstClass {
     public static void main(String[] args) {
-        System.out.println("Hello!!!");
+        System.out.println("Any text");
     }
 }
