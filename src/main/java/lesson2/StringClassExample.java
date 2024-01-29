4b1b694a14c71b71519882700451ae2a72af4dc4
Sergey <anduser@PLWRS-CND6212VLC.andersen-technology.by>
2024-01-29 19:55:55 +0300

    lesson2 check new keyword

---
+++ src/main/java/lesson2/StringClassExample.java
@@ -0,0 +1,14 @@
+package lesson2;
+
+public class StringClassExample {
+    public static void main(String[] args) {
+        int x = 8;
+        String text = new String("Hello world!!!");
+
+        String text2 = "Hello world!!";
+        System.out.println(text2);
+        String text3 = text2 + " Hello again!!";
+        System.out.println(text3 + 5);
+        System.out.println(text3);
+    }
+}
