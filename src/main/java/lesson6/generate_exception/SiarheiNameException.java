0b6502037e90732fe6cbc86f2a27be7de1dfd3ce
Sergey <anduser@PLWRS-CND6212VLC.andersen-technology.by>
2024-02-12 21:02:40 +0300

    lesson6

---
+++ src/main/java/lesson6/generate_exception/SiarheiNameException.java
@@ -0,0 +1,6 @@
+package lesson6.generate_exception;
+
+public class SiarheiNameException extends Exception{
+    @Override
+    public String getMessage() { return "Don't input Siarhei value!!!";}
+}
